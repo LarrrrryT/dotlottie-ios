2bc9f9f1f9092ef343e46d607f0109eb0e415ac7
Larry <larrytran555@gmail.com>
2022-02-01 12:08:00 -0800

    bumps spm min version to ios 14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "dotLottie",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v14),
         .macOS(.v10_12),
         .tvOS(.v9)
     ],
